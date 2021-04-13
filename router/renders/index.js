35979ce214e1ad636081362ab8053931d043ba39
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2021-04-13 16:53:12 +0200

    :link: Connected API and showing images to users

--- router/renders/index.js
+++ router/renders/index.js
@@ -1,14 +1,27 @@
 // Externals
 const express = require('express');
 const app = express();
+fetch = require('node-fetch');
 
 // Middleware
 app.set('view engine', 'ejs');
 
+// Internals
+const url = process.env.URL;
+const key = process.env.KEY;
+const limit = process.env.LIMIT;
+
 const index = (req, res) => {
-  res.render('index', {
-    headTwo: 'hoi head two'
-  });
+  fetch(`${url}?key=${key}&ps=${limit}`)
+    .then(async response => {
+      const data = await response.json()
+      const artList = data.artObjects
+
+      res.render('index', {
+        headTwo: 'hoi head two',
+        artList: artList
+      });
+    });
 };
 
 module.exports = index;
