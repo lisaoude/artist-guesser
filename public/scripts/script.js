8cfff139e4603011adf3de2977a3c72e9a102e1b
Lisa Oude Elferink <lisaoudeelferink@hotmail.com>
2021-04-26 14:46:17 +0200

    :broom: Organized & Cleaned up code

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,37 +1,21 @@
-// variables for chat
+//___ VARIABLES FOR DISPLAY NAME ___//
+const urlParams = new URLSearchParams(window.location.search);
+let username = urlParams.get('name');
+
+//___ VARIABLES FOR CHAT ___//
 const socket = io()
 const form = document.querySelector('#chatForm')
 const message = form.querySelector('input#message')
 const messages = document.querySelector('#messages')
 
-// variables for display name
-const urlParams = new URLSearchParams(window.location.search);
-const username = urlParams.get('name');
-
-//variables for images
+//___ VARIABLES FOR API DATA ___//
 const picture = document.querySelector('img')
 const text = document.querySelector('h2')
 
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  if (message.value) {
-    socket.emit('message', {
-      username,
-      message: message.value
-    })
-    // username.style.display = 'none'
-    message.value = ''
-  }
-})
-
-
-// send username to server
+//___ USER CONNECTED ___//
 socket.emit('userConnected', username);
-
 socket.on('userConnected', (username) => {
-
-  console.log('hallo??????')
 
   const joinMessage = document.createElement('p');
 
@@ -39,8 +23,19 @@ socket.on('userConnected', (username) => {
   messages.appendChild(joinMessage);
 })
 
+//___ CHAT 1 ___//
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  if (message.value) {
+    socket.emit('message', {
+      username,
+      message: message.value
+    })
+    message.value = ''
+  }
+})
 
-
+//___ CHAT 2 ___//
 socket.on('message', ({ message, username }) => {
   const element = document.createElement('li')
 
@@ -57,7 +52,19 @@ socket.on('message', ({ message, username }) => {
 })
 
 
+//___ API DATA ___//
 socket.on('showData', (artData) => {
   picture.src = artData.image;
   text.innerText = artData.text;
 })
+
+
+//___ DISCONNECT ___//
+socket.on('disconnected', (username) => {
+  const disconnectMessage = document.createElement('p');
+  disconnectMessage.innerText = `${username} has left the game`;
+  messages.appendChild(disconnectMessage);
+})
+
+
+console.log(username)
